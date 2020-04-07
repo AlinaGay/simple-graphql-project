e57a9f20c16dc7f76fb4c20fe9fa3a85a6bc7857
Alina Gay <gayfullina.a@gmail.com>
2020-04-07 11:50:06 +0300

    fixed mistakes

--- client/src/components/form.tsx
+++ client/src/components/form.tsx
@@ -1,49 +1,15 @@
 import React, { PureComponent } from 'react';
-import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
-import {UserDetails, Variables} from '../types';
-
-import './user.css';
 import User from './user';
 
-type State = {
-    textQuery: string;
-};
+import './form.css';
 
-export class Form extends PureComponent {
-
-    state: State = {
-        textQuery: `query { me { id  }}`
-    };
-
-    generateQuery = (checkboxNameChecked: boolean, checkboxPatronymicChecked: boolean, checkboxSurnameChecked: boolean) => 
-    `query { me { id ${checkboxNameChecked ? 'name ' : ''}` +
-    `${checkboxPatronymicChecked ? 'patronymic ' : ''}` +
-    `${checkboxSurnameChecked ? 'surname' : ''} }}`;
+export class Form extends React.PureComponent {
 
     checkboxName = React.createRef<HTMLInputElement>();
     checkboxPatronymic = React.createRef<HTMLInputElement>();
     checkboxSurname = React.createRef<HTMLInputElement>();
 
-    handleClick = () => {
-        const customStringQuery = this.generateQuery(this.checkboxName.current && this.checkboxName.current.checked ? true: false,
-            this.checkboxPatronymic.current && this.checkboxPatronymic.current.checked ? true: false,
-            this.checkboxSurname.current && this.checkboxSurname.current.checked ? true: false);
-
-        
-        this.setState({textQuery: customStringQuery});
-
-        console.log(this.state.textQuery);
-    }
-
-    componentDidMount(){
-        this.handleClick();
-        console.log('yes');
-    }
-
     render(){
-        
-        const { textQuery} = this.state;
         return(
             <fieldset>
                 <legend>Выбери поле</legend>
@@ -68,8 +34,11 @@ export class Form extends PureComponent {
                     />
                     Фамилия
                 </label>
-                <button onClick={this.handleClick}>Показать</button>
-                <User query={gql([textQuery])} />
+                <User
+                checkboxName={this.checkboxName}
+                checkboxPatronymic={this.checkboxPatronymic}
+                checkboxSurname={this.checkboxSurname}
+                />
             </fieldset>
         );
     }
--- client/src/components/user.tsx
+++ client/src/components/user.tsx
@@ -5,31 +5,76 @@ import {UserDetails, Variables} from '../types';
 
 import './user.css';
 
-function User(props: any) { 
+type State = {
+    textQuery: string;
+};
 
-    const textInput = React.createRef<HTMLInputElement>();
+type Props = {
+    checkboxName: React.RefObject<HTMLInputElement>;
+    checkboxPatronymic: React.RefObject<HTMLInputElement>;
+    checkboxSurname: React.RefObject<HTMLInputElement>;
+}
 
-    return (
-        <Query<UserDetails, Variables>
-            query={props.query}
-        >
-            {({ loading, error, data }) => { 
-                let res = '';
-                if(data && data.me && data.me.name){res+=`${data.me.name} `}
-                if(data && data.me && data.me.patronymic){res+=`${data.me.patronymic} `}
-                if(data && data.me && data.me.surname){res+=`${data.me.surname}`}
-                if (loading) return <div>"Loading..."</div>;
-                if (error) return <div>`Error!`</div>;
-                
-                return (
-                    <input
-                        type="text"
-                        value={res}
-                    />
-                );
-            }}
-        </Query>
-    );
+const generateQuery = (checkboxNameChecked: boolean | null, checkboxPatronymicChecked: boolean | null, checkboxSurnameChecked: boolean | null) : string => 
+    `query { me { id ${checkboxNameChecked ? 'name ' : ''}` +
+    `${checkboxPatronymicChecked ? 'patronymic ' : ''}` +
+    `${checkboxSurnameChecked ? 'surname' : ''} }}`;
+
+export class User extends React.PureComponent<Props>{ 
+
+    state: State = {
+        textQuery: `query { me { id  }}`
+    };
+
+    textInput = React.createRef<HTMLInputElement>();
+    
+    handleClick = (): void => {
+        const isCheckboxNameChecked: boolean | null= this.props.checkboxName && this.props.checkboxName.current && this.props.checkboxName.current.checked;
+        const isCheckboxPatronymicChecked: boolean | null = this.props.checkboxPatronymic && this.props.checkboxPatronymic.current && this.props.checkboxPatronymic.current.checked;
+        const isCheckboxSurnameChecked: boolean | null = this.props.checkboxSurname && this.props.checkboxSurname.current && this.props.checkboxSurname.current.checked;
+
+        const customStringQuery = generateQuery(isCheckboxNameChecked, 
+            isCheckboxPatronymicChecked, isCheckboxSurnameChecked);
+        
+            this.setState({textQuery: customStringQuery});
+    }
+
+    componentDidMount(){
+            this.handleClick();
+        }
+
+    render(){
+
+        const isCheckboxNameChecked: boolean | null = this.props.checkboxName && this.props.checkboxName.current && this.props.checkboxName.current.checked;
+        const isCheckboxPatronymicChecked: boolean | null = this.props.checkboxPatronymic && this.props.checkboxPatronymic.current && this.props.checkboxPatronymic.current.checked;
+        const isCheckboxSurnameChecked: boolean | null = this.props.checkboxSurname && this.props.checkboxSurname.current && this.props.checkboxSurname.current.checked;
+
+        return (
+            <Query<UserDetails, Variables>
+                query={gql([generateQuery(isCheckboxNameChecked, isCheckboxPatronymicChecked, isCheckboxSurnameChecked)])}
+            >
+                {({ loading, error, data }) => { 
+                    let res: string = '';
+                    if(data && data.me && data.me.name){res+=`${data.me.name} `}
+                    if(data && data.me && data.me.patronymic){res+=`${data.me.patronymic} `}
+                    if(data && data.me && data.me.surname){res+=`${data.me.surname}`}
+                    if (loading) return <div>"Loading..."</div>;
+                    if (error) return <div>`Error!`</div>;
+                    
+                    return (
+                        <div>
+                            <button onClick={this.handleClick}>Показать</button>
+                            <input
+                                type="text"
+                                value={res}
+                                className="text-input"
+                            />
+                        </div>
+                    );
+                }}
+            </Query>
+        );
+    }
 }
 
 export default User;
